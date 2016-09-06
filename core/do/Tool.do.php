b748fc2f404b6c250c966b8b04c063bd5798565a
Ltre Oreki <Ltre@users.noreply.github.com>
2016-09-06 22:40:48 +0800

    1、备用的高级加密路由REQUEST参数，方便HTML表单或有需要保护API的场景使用，该方式可防止被社工搜索到API源码

--- core/do/Tool.do.php
+++ core/do/Tool.do.php
@@ -66,36 +66,3 @@ function removeCookieLive($name)
     return setcookie($name, NULL, -1);
 }
 
-
-function ltreCrypt($str){
-    $table = str_split('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@~!*()-_.\'');//@为%的替代符
-    $v1 = str_replace('%', '@', urlencode($str));
-    $v2 = array();
-    foreach (str_split($v1) as $k => $v) {
-        $rawPos = array_search($v, $table);
-        $offset = floor(mt_rand(0, count($table)-$rawPos-1));
-        $plusPos = $rawPos + $offset;
-        $v2[] = $table[$offset];
-        $v2[] = $table[$plusPos];
-    }
-    $v2 = implode('', array_reverse($v2));
-    return $v2;
-}
-
-
-function ltreDeCrypt($str){
-    $table = str_split('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@~!*()-_.\'');
-    $rawList = array();
-    $offsetList = array();
-    foreach (array_reverse(str_split($str)) as $k => $v) {
-        $pos = array_search($v, $table);
-        if (intval(($k + 1) % 2) == 1) {
-            $offsetList[] = $pos;
-        } else {
-            $rawPos = intval($pos - $offsetList[($k + 1) / 2 - 1]);
-            @$rawList[] = $table[$rawPos];
-        }
-    }
-    $raw = str_replace('@', '%', implode('', $rawList));
-    return urldecode($raw);
-}
