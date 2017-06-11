decab3287b0aa72c3ff2b795776f62cd34bd5bf7
Ltre Oreki <Ltre@users.noreply.github.com>
2017-06-11 12:15:10 +0800

    BookPlus免生成分片文件

--- core/do/Book.do.php
+++ core/do/Book.do.php
@@ -4,10 +4,24 @@ class BookDo extends DIDo {
 
     function start(){
         dump(Book::getList());
+        dump(BookPlus::getList());
     }
 
 
     function read($code, $p = 1){
+        $data = BookPlus::read($code, $p);
+        if ($data) {
+            echo '<div style="width: 720px; overflow: auto; font-size: 28px;">';
+            echo preg_replace('/\r\n|\r|\n/', '<br><br>', $data['s']);
+            echo '</div>';
+            $lastP = $data['p'] > 1 ? $data['p'] - 1 : 1;
+            $nextP = $data['p'] + 1;
+            echo "<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"/read/{$code}/{$lastP}\">上一页</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"/read/{$code}/{$nextP}\">下一页</a><br><br><br><br><br><br><br><br><br>";
+        }
+    }
+
+
+    function read2($code, $p = 1){
         $data = Book::read($code, $p);
         if ($data) {
             echo '<div style="width: 720px; overflow: auto; font-size: 28px;">';
@@ -21,6 +35,20 @@ class BookDo extends DIDo {
 
 
     function prepare(){
+        ignore_user_abort();
+        set_time_limit(0);
+        $pageStart = arg('pageStart', 1); //保存的开始页（有个BUG，如果大于1，可能取到的字符串开头会有不完整的UTF-8字符，所以忽略该参数吧）
+        $pageNum = arg('pageNum', 1); //保存的页数（传0则保存所有页码的分片）
+        $per = arg('per', 4096); //每页多少字符
+        $url = arg('url', 'http://res.miku.us/res/other/ditieguishi.txt');
+        $name = '地铁诡事';
+        if ($url) {
+            BookPlus::prepare($name, $url, $pageStart, $pageNum, $per);
+        }
+    }
+    
+    
+    function prepare2(){
         ignore_user_abort();
         set_time_limit(0);
         $pageStart = arg('pageStart', 1); //保存的开始页（有个BUG，如果大于1，可能取到的字符串开头会有不完整的UTF-8字符，所以忽略该参数吧）
--- core/entity/BookPlus.php
+++ core/entity/BookPlus.php
@@ -1,35 +1,36 @@
 <?php
 
-class Book extends DIEntity {
+class BookPlus extends DIEntity {
 
-    //准备资源：下载文本，切分为多个分片，保存到指定目录
+    //准备资源：下载文本，保存分片对应的起始文件指针
     static function prepare($name, $url, $pageStart = 1, $pageNum = 1, $per = 1024){ //设置pageNum为0时，则一直读到文件尾部
-        $saveFolder = sha1($name.$url.$per);//原：sha1($url)
-        $saveName = $saveFolder . '.txt';
+        $code = sha1($name.$url.$per);
+        $saveName = $code . '.txt';
         $file = self::_download($url, $saveName);
         $h = fopen($file, 'rb');
-        self::_saveInfoMap($file, $saveFolder, $name, $url);
-        self::_createChunkFolder($file, $saveFolder);
+        $chunks = array();
         for ($p = $pageStart; ($pageNum == 0 ? true : ($p <= $pageNum)) && ! feof($h); $p ++) {
-            $s = self::_getChunkStr($h, $per);
-            self::_saveChunk($saveFolder, $p, $s);
+            $chunks[$p] = self::_getChunkInfo($h, $per);
         }
+        self::_saveInfoMap($code, $file, $name, $url, $chunks);
         fclose($h);
     }
 
 
-    //根据指定资源编码和页码，获取对应目录的对应分片
+    //根据指定资源编码和页码，获取对应资源的对应分片
     static function read($code, $p = 1){
-        $mapFile = DI_CACHE_PATH . 'books.map';
+        $mapFile = DI_CACHE_PATH . 'bookplus.map';
         @$json = file_get_contents($mapFile) ?: '{}';
         $map = json_decode($json, 1);
         if (isset($map[$code])) {
             $data = $map[$code];
-            $file = DI_CACHE_PATH . "{$data['folder']}/{$p}.txt";
-            @$s = file_get_contents($file) ?: '';
+            list($offset, $limit) = $data['chunks'][$p] ?: [1, 4096];
+            $s = self::_getChunk($data['file'], $offset, $limit);
             return array(
                 's' => $s,
                 'p' => $p,
+                'offset' => $offset,
+                'limit' => $limit,
                 'name' => $data['name'],
             );
         }
@@ -38,13 +39,14 @@ class Book extends DIEntity {
 
 
     static function getList(){
-        $mapFile = DI_CACHE_PATH . 'books.map';
+        $mapFile = DI_CACHE_PATH . 'bookplus.map';
         @$json = file_get_contents($mapFile) ?: '{}';
         return json_decode($json, 1);
     }
 
 
-    static private function _getChunkStr($h, $limit){
+    static private function _getChunkInfo($h, $limit){
+        $offset = ftell($h);
         $s = fread($h, $limit);
         $len = strlen($s);
         $last3 = strlen($s) >= 3 ? substr($s, -3) : $s;
@@ -61,10 +63,10 @@ class Book extends DIEntity {
         }
         //到达尾部后，检测最后三个字符是否存在无法拼成UTF-8的字符，有则舍弃之，并按所舍弃位数回退文件指针
         if (in_array($noAsciiCount, array(1, 2))) {
-            $s = substr($s, 0, strlen($s) - $noAsciiCount);
+            $limit -= $noAsciiCount;
             fseek($h, - $noAsciiCount, SEEK_CUR);
         }
-        return $s;
+        return [$offset, $limit];
     }
 
 
@@ -79,26 +81,22 @@ class Book extends DIEntity {
     }
 
 
-    static private function _saveInfoMap($file, $folder, $name, $url){
-        $mapFile = DI_CACHE_PATH . 'books.map';
+    static private function _saveInfoMap($code, $file, $name, $url, $chunks){
+        $mapFile = DI_CACHE_PATH . 'bookplus.map';
         $json = file_exists($mapFile) ? (file_get_contents($mapFile) ?: '{}') : '{}';
         $map = json_decode($json, 1);
-        $map[$folder] = array('file' => $file, 'folder' => $folder, 'name' => $name, 'url' => $url);
+        $map[$code] = array('file' => $file, 'name' => $name, 'url' => $url, 'chunks' => $chunks);
         file_put_contents($mapFile, json_encode($map, JSON_UNESCAPED_UNICODE));
     }
 
 
-    static private function _createChunkFolder($file, $folder){
-        $folder = DI_CACHE_PATH . $folder;
-        if (! file_exists($folder)) mkdir($folder, 0777);
+    static private function _getChunk($file, $offset, $limit){
+        $h = fopen($file, 'rb');
+        fseek($h, $offset, SEEK_SET);
+        $s = fread($h, $limit);
+        fclose($h);
+        return $s;
     }
 
-
-    static private function _saveChunk($folder, $p, $s){
-        $file = DI_CACHE_PATH . "{$folder}/{$p}.txt";
-        $hW = fopen($file, 'wb');
-        fwrite($hW, $s);
-        fclose($hW);
-    }
 
 }
